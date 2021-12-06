f7591c13aab83f6dd4307e31c7b653f99ab16c9e
Yohann Mansiaux <y.mansiaux@bordeaux-metropole.fr>
2021-12-06 10:27:03 +0100

    Commit initial

---
+++ dev_history.R
@@ -0,0 +1,31 @@
+usethis::use_build_ignore(files = "dev_history.R")
+usethis::use_gpl3_license()
+
+usethis::use_vignette("xtradataZZZ")
+
+usethis::use_pipe()
+
+usethis::use_package("httr")
+usethis::use_package("assertthat")
+usethis::use_package("glue")
+usethis::use_package("purrr")
+usethis::use_package("jsonlite")
+usethis::use_package("curl")
+
+
+
+usethis::use_testthat()
+
+usethis::use_test(name = "check_internet")
+usethis::use_test(name = "check_API_results")
+usethis::use_test(name = "xtradata_requete_features")
+usethis::use_test(name = "xtradata_requete_aggregate")
+usethis::use_test(name = "get_latitude_longitude")
+
+#
+vignettes <- TRUE
+devtools::check()
+devtools::build(vignettes = vignettes)
+devtools::install(build_vignettes = vignettes)
+
+pkgload::load_all()
